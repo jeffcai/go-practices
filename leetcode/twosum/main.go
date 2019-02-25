6a7396b6dc2158b537b03526c0c33d860a52b401
Jeff Cai <jfcai@cn.ibm.com>
2019-02-25 22:04:09 +0800

    completed the twosum problem.

--- leetcode/twosum/main.go
+++ leetcode/twosum/main.go
@@ -2,28 +2,62 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func main() {
 	nums := []int{2, 7, 11, 15}
 	fmt.Println(twoSum(nums, 9))
+	fmt.Println(twoSum1(nums, 9))
+	fmt.Println(twoSum2(nums, 9))
 
 	nums = []int{3, 2, 4}
 	fmt.Println(twoSum(nums, 6))
+	fmt.Println(twoSum1(nums, 6))
+	fmt.Println(twoSum2(nums, 6))
 
+	nums = []int{3, 3}
+	fmt.Println(twoSum(nums, 6))
+	fmt.Println(twoSum1(nums, 6))
+	fmt.Println(twoSum2(nums, 6))
 }
 
-//https://leetcode.com/problems/two-sum/solution/
+// https://leetcode.com/problems/two-sum/solution/
 // Brute Force appraoch with o(n*n)/o(1)
 func twoSum(nums []int, target int) []int {
 	for index, value := range nums {
 		for after, val := range nums[index+1 : len(nums)] {
-			fmt.Println(strconv.Itoa(index) + " " + strconv.Itoa(after))
 			if value+val == target {
 				return []int{index, after + index + 1}
 			}
 		}
 	}
-	return []int{0, 0}
+	panic("no elements found")
+}
+
+// Two-pass Hash Table with o(n)/o(n)
+func twoSum1(nums []int, target int) []int {
+	indexValMap := map[int]int{}
+	for index, value := range nums {
+		indexValMap[value] = index
+	}
+	for index, value := range nums {
+		found := indexValMap[target-value]
+		if found > 0 && found != index {
+			return []int{index, found}
+		}
+	}
+	panic("no elements found")
+}
+
+// One-pass Hash Table with o(n)/o(n)
+func twoSum2(nums []int, target int) []int {
+	indexValMap := map[int]int{}
+	for index, value := range nums {
+		idx, found := indexValMap[target-value]
+		if found {
+			return []int{idx, index}
+		}
+		indexValMap[value] = index
+	}
+	panic("no elements found")
 }
